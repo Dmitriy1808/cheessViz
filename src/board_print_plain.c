690d67f6ebcd3b15ca45ab98ab0da748d918d6f4
Dmitriy1808 <dmitrij.99@yandex.ru>
2018-05-31 01:46:58 +0700

    Changed 'board_print_plain.c'

--- src/board_print_plain.c
+++ src/board_print_plain.c
@@ -1,7 +1,7 @@
-#include "board-print-plain.h"
+#include "board_print_plain.h"
 #include <stdio.h>
 
-void board_print_plane(deck[8][8]){
+void board_print_plain(char deck[8][8]){
     for(i=0; i<8; ++i){
         for(j=0; j<8; ++j){
         printf("%c ", 8-j);
